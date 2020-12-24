deccfcc26686687d48d4157f9f7664e20908516a
spaenleh <basile.spaenlehauer@epfl.ch>
2020-12-24 17:08:31 +0100

    Lab4 not working

---
+++ lab4_project/hw/quartus/output_files/ES_mini_project_TRDB_D5M_LT24_time_limited.cdf
@@ -0,0 +1,15 @@
+/* Quartus Prime Version 18.1.0 Build 625 09/12/2018 SJ Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Ign)
+		Device PartName(SOCVHPS) MfrSpec(OpMask(0));
+	P ActionCode(Cfg)
+		Device PartName(5CSEMA4U23) Path("/home/vm/Desktop/shared/lab4_project/hw/quartus/output_files/") File("ES_mini_project_TRDB_D5M_LT24_time_limited.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
---
+++ lab4_project/sw/nios/application/obj/default/hello_world.d
@@ -0,0 +1,13 @@
+obj/default/hello_world.o: hello_world.c \
+ /home/vm/Desktop/shared/lab4_project/sw/nios/Lab_4_bsp/system.h \
+ /home/vm/Desktop/shared/lab4_project/sw/nios/Lab_4_bsp/linker.h \
+ /home/vm/Desktop/shared/lab4_project/sw/nios/Lab_4_bsp//HAL/inc/io.h \
+ /home/vm/Desktop/shared/lab4_project/sw/nios/Lab_4_bsp//HAL/inc/alt_types.h
+
+/home/vm/Desktop/shared/lab4_project/sw/nios/Lab_4_bsp/system.h:
+
+/home/vm/Desktop/shared/lab4_project/sw/nios/Lab_4_bsp/linker.h:
+
+/home/vm/Desktop/shared/lab4_project/sw/nios/Lab_4_bsp//HAL/inc/io.h:
+
+/home/vm/Desktop/shared/lab4_project/sw/nios/Lab_4_bsp//HAL/inc/alt_types.h:
